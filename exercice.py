e638343e5e7b6636dd526c36f87bcda3c2507c14
Eli-michaud <47183913+Eli-michaud@users.noreply.github.com>
2020-09-23 11:32:34 -0400

    test

--- exercice.py
+++ exercice.py
@@ -21,33 +21,48 @@ def use_prefixes() -> List[str]:
 
 
 def prime_integer_summation() -> int:
-    return 0
+    somme, nombreNombresPremiers, nombre = 2, 1, 3
+    while nombreNombresPremiers <= 100:
+        for nb in range(2, nombre):
+            if nombre % nb == 0:
+                nombre += 1
+                break
+            elif nb == (nombre - 1):
+                somme += nombre
+                nombreNombresPremiers += 1
+            else:
+                pass
+        nombre += 1
+    return somme
 
 
 def factorial(number: int) -> int:
     factorielle = 1
     i = number
-    while i > 0:
+    while i > 1:
         factorielle *= i
         i -= 1
     return factorielle
 
 
 def use_continue() -> None:
-    pass
+    for nb in range(1,11):
+        if nb == 5:
+            continue
+        print(nb)
 
 
 def main() -> None:
-    #print(f"La valeur absolue du nombre est {convert_to_absolute()}")
+    print(f"La valeur absolue du nombre est {convert_to_absolute()}")
 
-    #print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
+    print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
 
     print(f"La somme des nombres de 0 à 100 est: {prime_integer_summation()}")
 
     number = 10
-    #print(f"La factiorelle du nombre {number} est: {factorial(number)}")
+    print(f"La factiorelle du nombre {number} est: {factorial(number)}")
     
-    #print(f"L'affichage de la boucle est:")
+    print(f"L'affichage de la boucle est:")
     use_continue()
 
 
